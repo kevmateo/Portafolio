ad63625ca8d2008fa23bc381a0fc95c63b41758d
kevmateo <kevin.cano@epn.edu.ec>
2024-05-06 12:33:43 -0500

    experiencia tarjetas

--- src/componentes/experiencia.jsx
+++ src/componentes/experiencia.jsx
@@ -1,16 +1,16 @@
 
 function Experiencia({ ExperienciaNombre, ExperienciaInstitucion, ExperienciaFecha, ExperienciaDescripcion }) {
   return (
-    <div className='flex my-5 border '>
-      <div className='border'>
+    <article className='flex my-5 border p-4 shadow-3xl rounded-xl'>
+      <div className='w-1/3'>
         <p className='font-semibold text-sky-500 text-xl'>{ExperienciaNombre}</p>
         <p className='font-medium text-base'>{ExperienciaInstitucion}</p>
         <p className='text-base text-gray-600 font-thin'>{ExperienciaFecha}</p>
       </div>
-      <div className='border'>
+      <div className='w-2/3'>
         <p className="text-base text-gray-600 font-thin">{ExperienciaDescripcion}</p>
       </div>
-    </div>
+    </article>
   )
 }
 
